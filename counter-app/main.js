e96478d4249ace01238029575b100fbea8eee4b6
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2025-11-11 17:34:29 +0545

    save button added and styled

--- counter-app/main.js
+++ counter-app/main.js
@@ -1,5 +1,9 @@
-count=0;
+var count=0;
 function increment(){   
 count= count+1;
 document.getElementsByTagName('h1')[0].innerText=count; 
+}
+
+function save(){
+    console.log('saved:', count);
 }
