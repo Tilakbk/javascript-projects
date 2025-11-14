0647d344438696709bc1a8022e4e19aefaebc549
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2025-11-14 09:12:42 +0545

    Previous entries function added

--- counter-app/main.js
+++ counter-app/main.js
@@ -1,13 +1,18 @@
 let count=0;
-let rec="Previous entries: ";
-document.getElementsByClassName('text')[0].innerText=rec;
+let inc=document.getElementsByTagName('h1')[0]
+let saveEL= document.getElementsByClassName("text")[0]
 
 
 function increment(){   
 count= count+1;
-document.getElementsByTagName('h1')[0].innerText=count; 
+inc.innerText=count; 
 }
 
+
+
 function save(){
-    console.log('saved:', count);
+     let countStr= count+ " - "
+     saveEL.innerText += countStr
+
+    
 }
