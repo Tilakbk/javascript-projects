d808611f9af5738585f8180e5657bc22d3ce6fb9
Tilak Biswakarma <tilakbiswakarma56@gmail.com>
2025-11-17 19:42:04 +0545

    file added and logic checked

---
+++ black-jack/main.js
@@ -0,0 +1,16 @@
+let firstcard= 9
+let secondcard= 6
+
+let sum= firstcard + secondcard
+
+if(sum <21){
+    console.log("Do you want to draw a new card?")
+
+}
+else if(sum === 21){
+    console.log("wohoo! blackjack")
+}
+
+else{
+    console.log("you're out of the game!")
+}
